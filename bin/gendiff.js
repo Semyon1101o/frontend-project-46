987152a23118d24fd2b0c32a884dbca8ccc5fd28
Semyon1101o <146949498+Semyon1101o@users.noreply.github.com>
2024-12-10 10:27:56 +0000

    step 9

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,14 +5,15 @@ import genDiff from '../src/index.js';
 const program = new Command();
 
 program
-    .name('gendiff')
-    .version('0.1.0')
-    .description('Compares two configuration files and shows a difference.')
-    .argument('<filepath1>')
-    .argument('<filepath2>')
-    // .option('-f, --format <type>', 'output format')
-    .action((filepath1, filepath2) => {
-        console.log(genDiff(filepath1, filepath2));
-    })
+  .name('gendiff')
+  .version('0.1.0')
+  .description('Compares two configuration files and shows a difference.')
+  .argument('<filepat1>')
+  .argument('<filepath2>')
+  .option('-f, --format <type>', 'output format (default: "stylish")')
+  .action((filepath1, filepath2, option) => {
+		const result = genDiff(filepath1, filepath2, option.format);
+		console.log(result);
+  });
 
 program.parse();
--- src/index.js
+++ src/index.js
@@ -1,63 +1,16 @@
-import _ from 'lodash';
-import getData from './parseJson.js';
+import makeFormat from './formatters/index.js';
+import { getDifferentObject, readFile, getExtension } from './utils.js';
+import parse from './parsers.js';
 
-function getDifferentObject(obj1, obj2) {
-  const allKeys = _.sortBy(_.union(_.keys(obj1), _.keys(obj2)))
-    .map((key) => {
-      const oldValue = obj1[key];
-      const newValue = obj2[key];
-      if (!_.has(obj2, key)) {
-        return {
-          action: 'deleted',
-          key,
-          oldValue,
-        };
-      }
-      if (!_.has(obj1, key)) {
-        return {
-          action: 'added',
-          key,
-          newValue,
-        };
-      }
-      if (oldValue !== newValue) {
-        return {
-          action: 'changed',
-          key,
-          oldValue,
-          newValue,
-        };
-      }
-      return {
-        action: 'unchanged',
-        key,
-        oldValue,
-      };
-    })
-    .map((item) => {
-      const result = [];
-      if (item.action === 'deleted') {
-        result.push(`  - ${item.key}: ${item.oldValue}\n`);
-      }
-      if (item.action === 'unchanged') {
-        result.push(`    ${item.key}: ${item.oldValue}\n`);
-      }
-      if (item.action === 'changed') {
-        result.push(`  - ${item.key}: ${item.oldValue}\n`);
-        result.push(`  + ${item.key}: ${item.newValue}\n`);
-      }
-      if (item.action === 'added') {
-        result.push(`  + ${item.key}: ${item.newValue}`);
-      }
-      return result;
-    });
-  return `{\n${allKeys.flat().join('')}\n}`;
-}
-
-function genDiff(filepath1, filepath2) {
-  const dataFile1 = getData(filepath1);
-  const dataFile2 = getData(filepath2);
-  const result = getDifferentObject(dataFile1, dataFile2);
+function genDiff(filepath1, filepath2, format = 'stylish') {
+  const dataFile1 = readFile(filepath1);
+  const dataFile2 = readFile(filepath2);
+  const extension1 = getExtension(filepath1);
+  const extension2 = getExtension(filepath2);
+  const parseFile1 = parse(dataFile1, extension1);
+  const parseFile2 = parse(dataFile2, extension2);
+  const dataDiff = getDifferentObject(parseFile1, parseFile2);
+  const result = makeFormat(dataDiff, format);
   return result;
 }
 
